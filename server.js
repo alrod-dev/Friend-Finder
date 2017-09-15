06089c8a3d11b4f28bf54598c771aea655f19219
Alfredo Rodriguez <alrod909@gmail.com>
2017-09-15 13:59:55 -0600

    Repo Added

--- server.js
+++ server.js
@@ -61,6 +61,10 @@ app.get("/api/friends", function (req, res) {
     res.json(friendList);
 });
 
+app.get("/api/friends", function (req, res) {
+    res.json(friendList);
+});
+
 // Create New Characters - takes in JSON input
 app.post("/api/friends", function (req, res) {
 
