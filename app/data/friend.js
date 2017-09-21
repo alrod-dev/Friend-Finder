c2e3a3feb42d72a9ea4985c3e0434e7f1ea2f73b
Alfredo Rodriguez <alrod909@gmail.com>
2017-09-21 12:33:37 -0600

    Finally Done

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,55 +2,34 @@
 // File: JS - friend.js
 // Date: 09/16/2017
 
-// Capture the form inputs
-$(document).ready(function () {
+// Fake info for friendlist that exported towars the apiRoutes
+// =============================================================
+var friendList = [{
+    "name": "Ahmed",
+    "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+    "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+}, {
+    "name": "Jacob Deming",
+    "photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+    "scores": [4, 2, 5, 1, 3, 2, 2, 1, 3, 2]
+}, {
+    "name": "Jeremiah Scanlon",
+    "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+    "scores": [5, 2, 2, 2, 4, 1, 3, 2, 5, 5]
+}, {
+    "name": "Louis T. Delia",
+    "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+    "scores": [3, 3, 4, 2, 2, 1, 3, 2, 2, 3]
+}, {
+    "name": "Lou Ritter",
+    "photo": "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+    "scores": [4, 3, 4, 1, 5, 2, 5, 3, 1, 4]
+}, {
+    "name": "Jordan Biason",
+    "photo": "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+    "scores": [4, 4, 2, 3, 2, 2, 3, 2, 4, 5]
+}];
 
-    $("#submit").on("click", function () {
 
-        // Form validation
-        function validateForm() {
-            var isValid = true;
-
-            $(".form-control").each(function () {
-                if ($(this).val() === "")
-                    isValid = false;
-            });
-
-            return isValid;
-        }
-
-        // If all required fields are filled
-        if (validateForm() === true) {
-            // Create an object for the user's data
-            var userData = {
-                name: $("#name").val(),
-                photo: $("#photo").val(),
-                scores: [$("#q1").val(), $("#q2").val(), $("#q3").val(),
-                    $("#q4").val(), $("#q5").val(), $("#q6").val(),
-                    $("#q7").val(), $("#q8").val(), $("#q9").val(), $("#q10").val(),]
-            };
-
-
-            // Grab the URL of the website
-            var currentURL = window.location.origin;
-
-            // AJAX post the data to the friends API.
-            $.post(currentURL + "/api/friends", userData, function (data) {
-
-                // Grab the result from the AJAX post so that the best match's name and photo are displayed.
-                $("#matchName").text(data.name);
-                $('#matchImg').attr("src", data.photo);
-
-                // Show the modal with the best match
-                $("#resultsModal").modal('toggle');
-
-            });
-        }
-        else {
-            alert("Please fill out all fields before submitting!");
-        }
-
-        return false;
-    });
-
-});
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendList;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,12 +8,12 @@ var path = require("path");
 module.exports = function getSites(app){
 
     // Basic route that sends the user first to the AJAX Page
-//Main Page Route
+    //Main Page Route
     app.get("/", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/home.html"));
     });
 
-//Creating a friendList route
+    //Creating a friendList route
     app.get("/survey", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/survey.html"));
     });
